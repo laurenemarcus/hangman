fe02c4f059aec93ef47faed4892341139200cba3
Amy Michelle Johnson & Lauren Marcus <student@epicodus.com>
2015-02-03 15:19:28 -0800

    Take user to the win page if the game is won.

--- app.rb
+++ app.rb
@@ -42,14 +42,18 @@ post '/gallows/:id' do
   @word = Word.find(word_id)
   if @word.word.include?(@guess.guess) || @guess.save.eql?(false)
     @guess.update(included: true)
-    redirect("/gallows/#{@word.id}")
+    if @word.win?
+      redirect("/win/#{@word.id}")
+    else
+      redirect("/gallows/#{@word.id}")
+    end
   else
     @guess.update(included: false)
     redirect("/heads/#{@word.id}")
   end
 end
 
-get("/heads/:id") do
+get '/heads/:id' do
   @guesses= Guess.all()
   word_id = params.fetch("id").to_i
   @word = Word.find(word_id)
@@ -58,21 +62,25 @@ get("/heads/:id") do
   erb(:head)
 end
 
-post("/heads/:id") do
+post'/heads/:id' do
   guess = params.fetch("guess")
   word_id = params.fetch("id").to_i
   @guess = Guess.create(guess: guess, word_id: word_id)
   @word = Word.find(word_id)
   if @word.word.include?(@guess.guess) || @guess.save.eql?(false)
     @guess.update(included: true)
-    redirect("/heads/#{@word.id}")
+    if @word.win?
+      redirect("/win/#{@word.id}")
+    else
+      redirect("/heads/#{@word.id}")
+    end
   else
     @guess.update(included: false)
     redirect("/body/#{@word.id}")
   end
 end
 
-get("/body/:id") do
+get '/body/:id' do
   @guesses= Guess.all()
   word_id = params.fetch("id").to_i
   @word = Word.find(word_id)
@@ -81,21 +89,25 @@ get("/body/:id") do
 end
 
 
-post("/body/:id") do
+post '/body/:id' do
   guess = params.fetch("guess")
   word_id = params.fetch("id").to_i
   @guess = Guess.create(guess: guess, word_id: word_id)
   @word = Word.find(word_id)
   if @word.word.include?(@guess.guess) || @guess.save.eql?(false)
     @guess.update(included: true)
-    redirect("/body/#{@word.id}")
+    if @word.win?
+      redirect("/win/#{@word.id}")
+    else
+      redirect("/body/#{@word.id}")
+    end
   else
     @guess.update(included: false)
     redirect("/arms/#{@word.id}")
   end
 end
 
-get("/arms/:id") do
+get '/arms/:id' do
   @guesses= Guess.all()
   word_id = params.fetch("id").to_i
   @word = Word.find(word_id)
@@ -103,21 +115,25 @@ get("/arms/:id") do
   erb(:arms)
 end
 
-post("/arms/:id") do
+post'/arms/:id' do
   guess = params.fetch("guess")
   word_id = params.fetch("id").to_i
   @guess = Guess.create(guess: guess, word_id: word_id)
   @word = Word.find(word_id)
   if @word.word.include?(@guess.guess) || @guess.save.eql?(false)
     @guess.update(included: true)
-    redirect("/arms/#{@word.id}")
+    if @word.win?
+      redirect("/win/#{@word.id}")
+    else
+      redirect("/arms/#{@word.id}")
+    end
   else
     @guess.update(included: false)
     redirect("/legs/#{@word.id}")
   end
 end
 
-get("/legs/:id") do
+get '/legs/:id' do
   word_id = params.fetch("id").to_i
   @word = Word.find(word_id)
   @guesses = Guess.all
@@ -127,7 +143,12 @@ get("/legs/:id") do
   erb(:legs)
 end
 
-get("win/:id") do
+get '/win/:id' do
   word_id = params.fetch("id").to_i
   @word = Word.find(word_id)
+  @guesses = Guess.all
+  @guesses.each do |guess|
+    guess.delete
+  end
+  erb(:win)
 end
